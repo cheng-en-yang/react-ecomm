7dce5a672ffea7c1d91f1562cb529f6c4db377ff
MarcusMBP <marcusmbp@MarcusMBPde-MacBook-Pro.local>
2021-02-10 12:36:59 +0100

    added sign up with email and password

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import HomePage from '../src/pages/homepage/homepage.component.jsx';
 import ShopPage from './pages/shoppage/shop.component.jsx';
 import Header from './components/Header/header.component';
 import SingInAndSignUpPage from '../src/pages/sign-in-and-sign-up-page/sign-in-and-sign-up-page.component';
-import { auth } from '../src/firebase/fitebase.utils';
+import { auth, createUserProfileDocument } from '../src/firebase/fitebase.utils';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 class App extends React.Component {
@@ -19,8 +19,22 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if(userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapshot => {
+          this.setState({
+            currentUser: {
+              id: snapshot.id,
+              ...snapshot.data()
+            }
+          })
+          console.log(this.state)
+        })
+      } else {
+        this.setState({ currentUser: userAuth })
+      }
     })
   }
 
--- src/pages/sign-in-and-sign-up-page/sign-in-and-sign-up-page.component.jsx
+++ src/pages/sign-in-and-sign-up-page/sign-in-and-sign-up-page.component.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './sign-in-and-sign-up-page.styles.scss';
 import SignIn from '../../components/SignIn/sign-in.component';
+import SignUp from '../../components/Signup/sign-up.component';
 
 const SingInAndSignUpPage = () => (
     <div className='sign-in-and-sign-up'>
         <SignIn />
+        <SignUp />
     </div>
 )
 
