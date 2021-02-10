1271a003c77f6b753e3d7ad960a5ca16adfc8394
MarcusMBP <marcusmbp@MarcusMBPde-MacBook-Pro.local>
2021-02-10 22:10:10 +0100

    added dropdown cart and toggle hidden function

--- src/components/Header/header.component.jsx
+++ src/components/Header/header.component.jsx
@@ -4,8 +4,10 @@ import { Link } from 'react-router-dom';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import { auth } from '../../firebase/fitebase.utils';
 import { connect } from 'react-redux';
+import CartIcon from '../CartIcon/cart-icon.component';
+import CartDropdown from '../CartDropdown/cart-dropdown.component';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
     <div className='header'>
         <Link className='logo-container' to='/'>
             <Logo className='logo'/>
@@ -19,12 +21,18 @@ const Header = ({ currentUser }) => (
                 <Link className='option' to='/signin'>Sign In</Link>
             }
             <Link className='option' to='/contact'>Contact</Link>
+            <CartIcon />
         </div>
+        {
+            hidden ? null :
+            <CartDropdown />
+        }
     </div>
 )
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({user: {currentUser}, cart: {hidden}}) => ({
+    currentUser,
+    hidden
 })
 
 export default connect(mapStateToProps)(Header);
